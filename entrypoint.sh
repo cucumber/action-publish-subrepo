8033c03f91376924015cac130ca0b8f613335d6b
aurelien-reeves <aurelien.reeves@hiptest.net>
2022-08-18 12:00:05 +0200

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-TARGET_REPO=${GITHUB_REPOSITORY}-${INPUT_WORKING_DIRECTORY}
+TARGET_REPO=${GITHUB_REPOSITORY}-${INPUT_WORKING-DIRECTORY}
 echo target repo:"$TARGET_REPO"
 
 # Create the subtree split branch in pwd directory
-git subtree split --prefix="${INPUT_WORKING_DIRECTORY}" -b .split
+git subtree split --prefix="${INPUT_WORKING-DIRECTORY}" -b .split
 
 git config --global init.defaultBranch main
 git init .split
@@ -12,7 +12,7 @@ cd .split
 git config --global user.email "gitbot@github.com"
 git config --global user.name "$GITHUB_ACTOR"
 git pull ../ .split
-git remote add subrepo "git@github.com:${GITHUB_REPOSITORY}-${INPUT_WORKING_DIRECTORY}.git"
+git remote add subrepo "git@github.com:${GITHUB_REPOSITORY}-${INPUT_WORKING-DIRECTORY}.git"
 git pull subrepo main --allow-unrelated-histories
 git lfs pull subrepo main
 #push to subtree repo
