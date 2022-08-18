5a596b64ad45090d2ebdccc623c15abe6e4d40c5
Aurélien Reeves <aurelien.reeves@hiptest.net>
2022-08-18 15:56:32 +0200

    Rename '.split' into 'split' to remove any confusion

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ TARGET_REPO=${GITHUB_REPOSITORY}-${INPUT_WORKING_DIRECTORY}
 echo target repo:"$TARGET_REPO"
 
 # Create the subtree split branch in pwd directory
-git subtree split --prefix="${INPUT_WORKING_DIRECTORY}" -b .split
+git subtree split --prefix="${INPUT_WORKING_DIRECTORY}" -b split
 
 git config --global init.defaultBranch main
-git init .split
-cd .split
+git init split
+cd split
 git config --global user.email "gitbot@github.com"
 git config --global user.name "$GITHUB_ACTOR"
-git pull ../ .split
+git pull ../ split
 git remote add subrepo "git@github.com:${GITHUB_REPOSITORY}-${INPUT_WORKING_DIRECTORY}.git"
 git pull subrepo main --allow-unrelated-histories
 git lfs pull subrepo main
