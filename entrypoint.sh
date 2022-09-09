41fb12fb70619ad648c61203ccc9cce1b09583d1
Matt Wynne <matt@cucumber.io>
2022-09-09 11:03:20 -0700

    Attempt to use the new script from our entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,7 @@ token=$2
 create_subrepo_if_missing=$3
 
 # Check git token is set
-if [ -z "$token" ]
-then
+if [ -z "$token" ]; then
 	echo "Please set the github-token input"
 	exit 1
 fi
@@ -23,23 +22,9 @@ git config --global --add safe.directory /github/workspace
 git tag --list
 git show-ref
 
-# Set up git author
-git config --global user.email "gitbot@github.com"
-git config --global user.name "$GITHUB_ACTOR"
-
-# Get any tag pointing to current commit
-tag="$(git tag --points-at "$GITHUB_REF")"
-
-# Create the subtree split branch and pull into main branch of a new repo
-git subtree split --prefix="$working_directory" -b split
-monorepo=$(pwd)
-git init /tmp/split -b main
-cd /tmp/split
-git pull "$monorepo" split:main
-
 # Create subrepo if missing, or check access for token if it exists
 subrepo_url="https://$token@github.com/$subrepo_name"
-if ! curl --fail "$subrepo_url" > /dev/null; then
+if ! curl --fail "$subrepo_url" >/dev/null; then
 	if [ "$create_subrepo_if_missing" = 'true' ]; then
 		# TODO: consider using --template here to have a template for read-only subrepos
 		export GH_TOKEN="$token"
@@ -54,21 +39,12 @@ else
 	echo "Testing connection to subrepo $subrepo_url"
 	git ls-remote "$subrepo_url"
 	# pull is OK, what about push?
-	git checkout --orphan test-push
-	git reset --hard
+	pushd "$(mktemp -d)" || exit
+	git init -b main
 	git commit --allow-empty --message "test push"
 	git push "$subrepo_url" test-push:refs/test/push || (echo -e "Unable to push to remote repo $subrepo_url\nCheck your token's permissions." && exit 1)
 	git push "$subrepo_url" :refs/test/push
-	git checkout main
+	popd
 fi
 
-# tag the commit on main
-if [ -n "$tag" ]; then
-	git tag "$tag" main
-fi
-
-# Pull older commits from remote
-git pull "$subrepo_url" main --rebase
-
-# Push the main branch and any tags referencing its commits
-git push "$subrepo_url" main "$tag" --force
+"$(dirname "$0")"/publish-to-subrepo "$working_directory" "$subrepo_url"
